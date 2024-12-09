df94bcfffca03211f274e2df7a8ca8242d1053e0
amirhosein <amirhosein@cyberdesign.se>
2024-12-09 22:41:48 +0330

    feat: add Rating data model

--- rates/models.py
+++ rates/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -5,3 +6,10 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     rate_average = models.FloatField(default=0.0)
 
+
+class Rating(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.CharField()
+    score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
