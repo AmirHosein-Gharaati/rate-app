eca9440fa58db6eaa3b66e9c375df65088002eae
amirhosein <amirhosein@cyberdesign.se>
2024-12-12 16:30:57 +0330

    fix: fix bug in computing rating average and post ratings

--- rates/tasks.py
+++ rates/tasks.py
@@ -1,7 +1,4 @@
-import datetime
 from collections import defaultdict
-
-from django.db.models import QuerySet
 
 from rates.models import Post, Rating, RatingAverage
 from rates.services import calculate_weighted_average
@@ -18,7 +15,7 @@ def handle_computing_rating_averages():
 
     for post, rating_list in grouped_ratings.items():
         count = len(rating_list)
-        rating_sum = sum(rating.score for rating in ratings)
+        rating_sum = sum(rating.score for rating in rating_list)
         average = round(rating_sum / count, 2)
 
         rating_averages.append(
@@ -35,12 +32,18 @@ def handle_computing_rating_averages():
 def handle_updating_post_rating():
     posts = Post.objects.all()
 
-    for post in posts:
-        averages = RatingAverage.objects.filter(post=post).order_by('-from_time')
+    posts_to_update = []
 
-        if len(averages) > 0:
+    for post in posts:
+        averages = RatingAverage.objects.filter(post=post).order_by('-created_at')
+
+        if averages.exists():
             total_average = calculate_weighted_average(averages)
 
             post.rate_average = total_average
-            post.user_count = Rating.objects.filter(post=post).count()
-            post.save()
+            post.user_count = Rating.objects.filter(post=post, computed=True).count()
+
+            posts_to_update.append(post)
+
+    if posts_to_update:
+        Post.objects.bulk_update(posts_to_update, ['rate_average', 'user_count'])
