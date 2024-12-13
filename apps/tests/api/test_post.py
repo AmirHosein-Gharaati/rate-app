1f72bfb99f53ba0819af53161e110dfbb504899d
amirhosein <amirhosein@cyberdesign.se>
2024-12-13 20:35:14 +0330

    test: add test for inserting and getting posts

--- apps/tests/api/test_post.py
+++ apps/tests/api/test_post.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
@@ -5,10 +7,22 @@ from rest_framework.test import APIClient
 
 @pytest.mark.django_db
 def test_getting_posts():
+    # setup
     api_client = APIClient()
-
     url_ = reverse("api:post")
+    number_of_posts_to_insert = 10
 
-    response = api_client.get(url_, content_type="application/json")
+    for i in range(1, number_of_posts_to_insert + 1):
+        body = {"title": f"Post{i}"}
+        response = api_client.post(url_, json.dumps(body), content_type="application/json")
+        assert response.status_code == 201
 
-    assert response.status_code == 200
+    # when
+    posts_response = api_client.get(url_, content_type="application/json")
+
+    # then
+    assert posts_response.status_code == 200
+    assert posts_response.data['count'] == number_of_posts_to_insert
+    assert posts_response.data['limit'] == number_of_posts_to_insert
+    posts = posts_response.data['results']
+    assert posts[0]['id'] == 1
