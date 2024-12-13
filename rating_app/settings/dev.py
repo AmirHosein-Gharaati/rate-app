57fafcbb455b494e733ea836f7060356eae1c58c
amirhosein <amirhosein@cyberdesign.se>
2024-12-13 10:26:12 +0330

    feat: configure development environment and docker compose

--- rating_app/settings/dev.py
+++ rating_app/settings/dev.py
@@ -6,7 +6,24 @@ DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost'])
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ORIGIN_WHITELIST = env.list('CORS_ORIGIN_WHITELIST', default=[])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rate_app',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+        "OPTIONS": {
+            "pool": True,
+        },
+    }
+}
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
