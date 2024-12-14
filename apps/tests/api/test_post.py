04d3770e6fb620124ebc1f52c6d90bb0a0a5bc31
amirhosein <amirhosein@cyberdesign.se>
2024-12-14 11:22:22 +0330

    test: update test_post to assert pagination results

--- apps/tests/api/test_post.py
+++ apps/tests/api/test_post.py
@@ -10,7 +10,7 @@ def test_getting_posts():
     # setup
     api_client = APIClient()
     url_ = reverse("api:post")
-    number_of_posts_to_insert = 10
+    number_of_posts_to_insert = 100
 
     for i in range(1, number_of_posts_to_insert + 1):
         body = {"title": f"Post{i}"}
@@ -22,7 +22,6 @@ def test_getting_posts():
 
     # then
     assert posts_response.status_code == 200
-    assert posts_response.data['count'] == number_of_posts_to_insert
-    assert posts_response.data['limit'] == number_of_posts_to_insert
-    posts = posts_response.data['results']
-    assert posts[0]['id'] == 1
+    assert posts_response.data['offset'] == 0
+    assert posts_response.data['limit'] == 10
+    assert len(posts_response.data['results']) == 10
